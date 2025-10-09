a481824cdfdfd7fef7217a647c64ddb7db54c4a6
Mehmetalicvkl <32cevikel@gmail.com>
2025-10-09 23:03:50 +0300

    olusturuldu

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoMvc.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // bu, Views/User/Index.cshtml dosyasını arar
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToDomvs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
